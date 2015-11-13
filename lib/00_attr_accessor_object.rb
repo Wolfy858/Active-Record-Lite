073a41f5177d694d1232b03ca1c05393febf84ae
App Academy Student <student@appacademy.io>
2015-11-13 13:38:42 -0800

    finish my_attr_accessor

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -1,5 +1,20 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      define_method "#{name}" do
+        instance_variable_get("@#{name}")
+      end
+
+      define_method("#{name}=") do |object|
+        instance_variable_set("@#{name}", object)
+      end
+    end
   end
 end
+
+
+# class Cat
+#   def favorite_color=(color)
+#     @color = color
+#   end
+# end
